9fd2d409acbc9e72188037761defad20772f3c13
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-24 17:00:48 +0800

    feat: implement UML link types and mode handling
    Adds LinkType enum for defining relationship types between UML objects
    Enhances LinkMode to support association, generalization, and composition links
    Implements link creation logic with origin and destination objects
    
    The changes enable proper relationship management in the UML diagram system

---
+++ src/links/LinkType.java
@@ -0,0 +1,7 @@
+package links;
+
+public enum LinkType {
+    ASSOCIATION,
+    GENERALIZATION,
+    COMPOSITION
+}
--- src/modes/LinkMode.java
+++ src/modes/LinkMode.java
@@ -1,7 +1,49 @@
 package modes;
 
+import core.UMLManager;
+import links.AssociationLink;
+import links.BaseLink;
+import links.CompositionLink;
+import links.GeneralizationLink;
+import links.LinkType;
+import objects.BaseObject;
+
 public class LinkMode implements Mode {
+    private LinkType linkType;
+    private BaseObject origin;
+    private BaseObject destination;
+
+    public LinkMode(LinkType linkType) {
+        this.linkType = linkType;
+    }
+
+    public void setOrigin(BaseObject origin) {
+        this.origin = origin;
+    }
+
+    public void setDestination(BaseObject destination) {
+        this.destination = destination;
+    }
+
     public void handle() {
         System.out.println("LinkMode");
+
+        BaseLink link = null;
+
+        switch (linkType) {
+            case ASSOCIATION:
+                link = new AssociationLink(origin, destination);
+                break;
+            case GENERALIZATION:
+                link = new GeneralizationLink(origin, destination);
+                break;
+            case COMPOSITION:
+                link = new CompositionLink(origin, destination);
+                break;
+            default:
+                break;
+        }
+
+        UMLManager.getInstance().addLink(link);
     }
 }
