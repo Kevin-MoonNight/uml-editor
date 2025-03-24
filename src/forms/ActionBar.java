df5fa0eafca734897a58c7aab33085582f0ee08c
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-24 17:01:14 +0800

    feat: implement action button handlers in ActionBar
    Adds functionality to handle various UML diagram actions:
    - Mode switching for selection
    - Link creation (Association, Generalization, Composition)
    - Shape creation (Rectangle, Oval)
    
    Integrates with UMLManager to manage state transitions

--- src/forms/ActionBar.java
+++ src/forms/ActionBar.java
@@ -1,11 +1,24 @@
 package forms;
 
 import javax.swing.*;
+
+import core.UMLManager;
+import links.LinkType;
+import modes.CreateMode;
+import modes.LinkMode;
+import modes.SelectMode;
+import objects.ObjectType;
+
 import java.awt.*;
 
 public class ActionBar extends JToolBar {
     private static final String[] actions = {
-            "Select", "Association", "Generalization", "Composition", "Rect", "Oval"
+            "Select",
+            "Association",
+            "Generalization",
+            "Composition",
+            "Rect",
+            "Oval"
     };
 
     private static final Dimension size = new Dimension(100, Integer.MAX_VALUE);
@@ -24,7 +37,35 @@ public class ActionBar extends JToolBar {
     private void createActionButtons() {
         for (String action : actions) {
             ActionButton btn = new ActionButton(action);
+            btn.addActionListener(e -> handleAction(action));
             add(btn);
+        }
+    }
+
+    private void handleAction(String action) {
+        System.out.println("Action: " + action);
+
+        switch (action) {
+            case "Select":
+                UMLManager.getInstance().setMode(new SelectMode());
+                break;
+            case "Association":
+                UMLManager.getInstance().setMode(new LinkMode(LinkType.ASSOCIATION));
+                break;
+            case "Generalization":
+                UMLManager.getInstance().setMode(new LinkMode(LinkType.GENERALIZATION));
+                break;
+            case "Composition":
+                UMLManager.getInstance().setMode(new LinkMode(LinkType.COMPOSITION));
+                break;
+            case "Rect":
+                UMLManager.getInstance().setMode(new CreateMode(ObjectType.RECT));
+                break;
+            case "Oval":
+                UMLManager.getInstance().setMode(new CreateMode(ObjectType.OVAL));
+                break;
+            default:
+                break;
         }
     }
 }
