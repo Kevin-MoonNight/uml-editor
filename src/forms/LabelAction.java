b8d71cabb532eec628d523557ebad7e4379cdafe
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-25 02:11:11 +0800

    refactor: move label dialog logic to dedicated component
    Extracts label creation functionality from MenuBar into LabelAction class
    Improves code organization by encapsulating dialog handling logic
    Reduces code duplication and improves maintainability

--- src/forms/LabelAction.java
+++ src/forms/LabelAction.java
@@ -1,9 +1,23 @@
 package forms;
 
+import java.awt.Frame;
+
 import javax.swing.JMenuItem;
 
 public class LabelAction extends JMenuItem {
-    public LabelAction() {
+    public LabelAction(Frame owner) {
         super("Label");
+
+        addActionListener(e -> {
+            CustomLabelStyleDialog dialog = new CustomLabelStyleDialog(owner);
+            dialog.setVisible(true);
+            if (dialog.isConfirmed()) {
+                // Handle the dialog result here
+                String name = dialog.getName();
+                String shape = dialog.getSelectedShape();
+                String color = dialog.getSelectedColor();
+                int fontSize = dialog.getSelectedFontSize();
+            }
+        });
     }
 }
--- src/forms/MenuBar.java
+++ src/forms/MenuBar.java
@@ -8,26 +8,9 @@ public class MenuBar extends JMenuBar {
         JMenu fileMenu = new JMenu("File");
 
         JMenu editMenu = new JMenu("Edit");
-        createLabelMenuItem(owner, editMenu);
+        editMenu.add(new LabelAction(owner));
 
         this.add(fileMenu);
         this.add(editMenu);
-    }
-
-    private void createLabelMenuItem(Frame owner, JMenu editMenu) {
-        JMenuItem label = new JMenuItem("Label");
-        editMenu.add(label);
-
-        label.addActionListener(e -> {
-            CustomLabelStyleDialog dialog = new CustomLabelStyleDialog(owner);
-            dialog.setVisible(true);
-            if (dialog.isConfirmed()) {
-                // Handle the dialog result here
-                String name = dialog.getName();
-                String shape = dialog.getSelectedShape();
-                String color = dialog.getSelectedColor();
-                int fontSize = dialog.getSelectedFontSize();
-            }
-        });
     }
 }
