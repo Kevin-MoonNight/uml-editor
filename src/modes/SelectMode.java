831348b801bf953c1fccb762b2c3fd36ea8b5768
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-24 17:01:37 +0800

    feat: implement object selection functionality
    Adds single and multi-select capabilities to SelectMode:
    - Single click selects individual objects
    - Click and drag creates selection boundary
    - Selected objects are managed through UMLManager
    
    Improves user interaction by providing visual object selection tools

--- src/modes/SelectMode.java
+++ src/modes/SelectMode.java
@@ -1,7 +1,67 @@
 package modes;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.UMLManager;
+import objects.BaseObject;
+import objects.Boundary;
+import utils.BoundaryUtil;
+
 public class SelectMode implements Mode {
+    private Point origin;
+    private Point destination;
+
+    public void setOrigin(Point origin) {
+        this.origin = origin;
+    }
+
+    public Point getOrigin() {
+        return origin;
+    }
+
+    public void setDestination(Point destination) {
+        this.destination = destination;
+    }
+
+    public Point getDestination() {
+        return destination;
+    }
+
     public void handle() {
         System.out.println("SelectMode");
+        if (origin == null || destination == null) {
+            return;
+        }
+
+        if (origin.getX() == destination.getX() && origin.getY() == destination.getY()) {
+            singleSelect(origin);
+            return;
+        }
+
+        multiSelect();
+
+        this.origin = null;
+        this.destination = null;
+    }
+
+    private void singleSelect(Point point) {
+        var objects = UMLManager.getInstance().getObjects();
+
+        BaseObject selectObject = BoundaryUtil.getObjectsAtPoint(objects, point);
+        ArrayList<BaseObject> selectedObjects = selectObject != null ? new ArrayList<BaseObject>(List.of(selectObject))
+                : new ArrayList<BaseObject>();
+
+        UMLManager.getInstance().setSelectedObjects(selectedObjects);
+    }
+
+    private void multiSelect() {
+        var objects = UMLManager.getInstance().getObjects();
+
+        var boundary = new Boundary(origin.x, origin.y, destination.x - origin.x, destination.y - origin.y);
+        var selectObjects = BoundaryUtil.getObjectsInBoundary(objects, boundary);
+
+        UMLManager.getInstance().setSelectedObjects(selectObjects);
     }
 }
