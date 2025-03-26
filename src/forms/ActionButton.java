22e627b40ded67d9f32a45eb5da8a102057fb146
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-27 00:22:30 +0800

    feat: add icon support to action buttons
    Updates ActionButton component to load and display PNG icons instead of text
    Implements dynamic icon color updates based on button state:
    - Default state
    - Focus state
    - Hover state
    
    Icons added for:
    - Basic shapes (Rectangle, Oval)
    - UML relationships (Association, Composition, Generalization)
    - Selection tool

--- src/forms/ActionButton.java
+++ src/forms/ActionButton.java
@@ -3,6 +3,7 @@ package forms;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
 public class ActionButton extends JButton {
     private static final Color DEFAULT_BG = Color.WHITE;
@@ -11,6 +12,7 @@ public class ActionButton extends JButton {
     private static final Color FOCUSED_FG = Color.WHITE;
     private static final Color HOVER_BG = Color.LIGHT_GRAY;
     private static final Color HOVER_FG = Color.BLACK;
+    private ImageIcon originalIcon;
 
     public ActionButton(String name) {
         setContentAreaFilled(false);
@@ -21,19 +23,28 @@ public class ActionButton extends JButton {
         setFocusable(true);
         setBackground(DEFAULT_BG);
         setForeground(DEFAULT_FG);
-        setText(name);
+
+        try {
+            String iconPath = "icons/" + name + ".png";
+            originalIcon = new ImageIcon(iconPath);
+            updateIconColor(DEFAULT_FG);
+        } catch (Exception e) {
+            System.err.println("無法加載圖標: " + name);
+        }
 
         addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
                 setBackground(FOCUSED_BG);
                 setForeground(FOCUSED_FG);
+                updateIconColor(FOCUSED_FG);
             }
 
             @Override
             public void focusLost(FocusEvent e) {
                 setBackground(DEFAULT_BG);
                 setForeground(DEFAULT_FG);
+                updateIconColor(DEFAULT_FG);
             }
         });
 
@@ -42,6 +53,7 @@ public class ActionButton extends JButton {
             public void mouseEntered(MouseEvent e) {
                 setBackground(HOVER_BG);
                 setForeground(HOVER_FG);
+                updateIconColor(HOVER_FG);
             }
 
             @Override
@@ -49,9 +61,40 @@ public class ActionButton extends JButton {
                 if (!hasFocus()) {
                     setBackground(DEFAULT_BG);
                     setForeground(DEFAULT_FG);
+                    updateIconColor(DEFAULT_FG);
                 }
             }
         });
+    }
+
+    private void updateIconColor(Color color) {
+        if (originalIcon != null) {
+            Image image = originalIcon.getImage();
+            BufferedImage buffered = new BufferedImage(
+                    image.getWidth(null),
+                    image.getHeight(null),
+                    BufferedImage.TYPE_INT_ARGB);
+
+            Graphics2D g2d = buffered.createGraphics();
+            g2d.drawImage(image, 0, 0, null);
+            g2d.dispose();
+
+            for (int x = 0; x < buffered.getWidth(); x++) {
+                for (int y = 0; y < buffered.getHeight(); y++) {
+                    int rgba = buffered.getRGB(x, y);
+                    int alpha = (rgba >> 24) & 0xff;
+                    if (alpha != 0) {
+                        buffered.setRGB(x, y, new Color(
+                                color.getRed(),
+                                color.getGreen(),
+                                color.getBlue(),
+                                alpha).getRGB());
+                    }
+                }
+            }
+
+            setIcon(new ImageIcon(buffered));
+        }
     }
 
     @Override
