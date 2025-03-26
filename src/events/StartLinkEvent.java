a626840ef0e96c913a9248001d36741178d56121
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-26 19:44:07 +0800

    fix: add null check for target object in link event
    Prevents potential NullPointerException when attempting to set source point
    and source object in LinkMode if no valid object is found at click location

--- src/events/StartLinkEvent.java
+++ src/events/StartLinkEvent.java
@@ -12,6 +12,10 @@ public class StartLinkEvent {
     public static void handle(MouseEvent e, LinkMode linkMode) {
         var targetObject = BoundaryUtil.getObjectAtPoint(umlManager.getObjects(), e.getPoint());
 
+        if (targetObject == null) {
+            return;
+        }
+
         linkMode.setSource(targetObject);
         linkMode.setSourcePoint(e.getPoint());
     }
