c46c5c0a18da19d8282aaf1e747f5258bd34f2fd
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-27 00:30:22 +0800

    refactor: extract UML object size to constants
    Moves hardcoded object size value from CreateEvent to a dedicated UMLConstants class for better maintainability and reuse.
    
    Eliminates magic number by creating a centralized constant for UML object dimensions.

---
+++ src/constants/UMLConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public final class UMLConstants {
+    private UMLConstants() {
+    }
+
+    public static final int OBJECT_SIZE = 100;
+}
--- src/events/CreateEvent.java
+++ src/events/CreateEvent.java
@@ -2,14 +2,13 @@ package events;
 
 import java.awt.event.MouseEvent;
 
+import constants.UMLConstants;
 import modes.CreateMode;
 import objects.Boundary;
 
 public class CreateEvent {
-    private static final int DEFAULT_SIZE = 100;
-
     public static void handle(MouseEvent e, CreateMode createMode) {
-        createMode.setBoundary(new Boundary(e.getX(), e.getY(), DEFAULT_SIZE, DEFAULT_SIZE));
+        createMode.setBoundary(new Boundary(e.getX(), e.getY(), UMLConstants.OBJECT_SIZE, UMLConstants.OBJECT_SIZE));
         createMode.handle();
     }
 }
