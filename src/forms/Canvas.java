b64a075fdc1983859055487e6edfa7c363b6df30
Kevin-MoonNight <k0937477331@gmail.com>
2025-03-28 16:36:06 +0800

    refactor: reorganize Canvas component initialization
    Improves code organization by:
    - Reordering import statements for better readability
    - Renaming setup() method to more descriptive configureCanvas()
    - Moving canvas registration to constructor for clearer initialization flow

--- src/forms/Canvas.java
+++ src/forms/Canvas.java
@@ -1,27 +1,27 @@
 package forms;
 
-import javax.swing.*;
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+import constants.UMLConstants;
 import core.CanvasManager;
 import core.DrawerManager;
 import utils.DrawerUtil;
-import constants.UMLConstants;
-
-import java.awt.*;
 
 public class Canvas extends JPanel {
     private final CanvasManager canvasManager = CanvasManager.getInstance();
     private final DrawerManager drawerManager = DrawerManager.getInstance();
 
     public Canvas() {
-        setup();
-    }
-
-    public void setup() {
-        setLayout(null);
-        setBackground(UMLConstants.CANVAS_BG);
+        configureCanvas();
 
         canvasManager.setCanvas(this);
+    }
+
+    public void configureCanvas() {
+        setLayout(null);
+        setBackground(UMLConstants.CANVAS_BG);
     }
 
     @Override
